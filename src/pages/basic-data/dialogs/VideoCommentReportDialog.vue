a4245fa3a2de9504a13c52c7da7e83c817a2bbcb
gaoang <609767994@qq.com>
2018-09-06 14:49:06 +0800

    举报视频评论，在弹窗里面新增标记处理

--- src/pages/basic-data/dialogs/VideoCommentReportDialog.vue
+++ src/pages/basic-data/dialogs/VideoCommentReportDialog.vue
@@ -20,16 +20,31 @@
         <el-form-item label='视频文件 ：'>
         <icon-tag type="warning" icon="fa fa-info" v-if="dialog.detail.url === '' || dialog.detail.img === null">无视频</icon-tag>
         <video :src='dialog.detail.url' class='vide-report-item' controls />
-        </el-form-item>
+        </el-form-item>  
       </el-form>
     </div>
+    <span  slot="footer" class="item" effect="dark"  placement="top" slot-scope="scope" >
+      <template >
+      <el-button type="warning"  size="small" @click="processConfirm(scope.row)" :disabled='Handle'>
+      <i class="fa fa-check"></i>标记处理
+      </el-button>
+      </template>
+      <el-button type="primary" @click="show = false" size="small">关闭</el-button>
+    </span>
     <span slot="footer">
       <el-button type="primary" @click="show = false" size="small">关闭</el-button>
     </span>
   </el-dialog>
 </template>
 <script>
-import { getVideoCommentdetail } from '../../../api/basic-data/report-info';
+import { 
+   updateReportInfo,
+   getVideoCommentdetail 
+  } from '../../../api/basic-data/report-info';
+import {
+  PROMPT_MSG_REG_EXPRESSION,
+  REPORT_TYPE_LIST
+  } from '../../../utils/constants';
 export default {
   data() {
     return {
@@ -46,11 +61,14 @@ export default {
           createTime: null,
           videoId: null,
           url: null,
+          reportId: null,
+          processed: null,
           show: false,
           formLoading: true,
           btnLoading: false
         }
-      }
+      },
+      Handle: false
     };
   },
   methods: {
@@ -65,14 +83,42 @@ export default {
           this.dialog.detail.createTime = data.detail.createTime;
           this.dialog.detail.videoId = data.detail.videoId;
           this.dialog.detail.url = data.detail.url;
-
+          this.dialog.detail.reportId = data.detail.reportId;
+          this.dialog.detail.processed = data.detail.processed;
           this.dialog.detail.formLoading = false;
+          if(this.dialog.detail.processed=== 1){
+            this.Handle = true;
+          }else if(this.dialog.detail.processed=== 0){
+            this.Handle=false;
+          }
+          ;
         })
         .catch(error => {
           this.$message.error('视频评论详情不存在');
           this.show = false;
         });
-    }
+    },
+    processConfirm(row) {
+      this.$prompt('请输入处理结果描述', '处理举报信息', {
+        inputPattern: PROMPT_MSG_REG_EXPRESSION,
+        inputErrorMessage: '请输入1-20字处理结果描述'
+      })
+        .then(({ value }) => {
+          let msg = {
+            reportId: this.dialog.detail.reportId,
+            memo: value
+          };
+          updateReportInfo(msg)
+            .then(resp => {
+              this.$message.success('操作成功');           
+              this.Handle = true;
+              this.$emit('done');
+              this.show = false;
+            })  
+            .catch(errorMsg => {});
+        })
+        .catch(() => {});
+    },
   }
 };
 </script>
