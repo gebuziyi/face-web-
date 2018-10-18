e5b972729db1794f8785f2101ff9cccf3b1d7723
wenjian <1639301503@qq.com>
2018-10-18 20:01:17 +0800

    视频话题数量改为可点击,点击后弹出视频列表 弹窗OK, 视频列表还需要再获取

---
+++ src/pages/video/dialogs/VideoTopicVideoDialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-dialog :visible.sync="show" :title="`#${theTopic.tname} - 视频列表`" width="1200px" :close-on-click-modal="false">
+    {{ theTopic.topicId }}
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: 'video-topic-video-dialog',
+
+  data() {
+    return {
+      theTopic: {
+        tname: '',
+        topicId: null
+      },
+      show: false
+    };
+  },
+
+  methods: {
+    showDialog(row) {
+      this.theTopic.tname = row.tname;
+      this.theTopic.topicId = row.topicId;
+      this.show = true;
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
