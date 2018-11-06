39361fe280a88b57933f441d028efbaa56f6eae9
qkb <1464645636@qq.com>
2018-11-06 14:22:36 +0800

    仪表盘：新增用户添加userId

--- src/pages/dashboard/tables/LatestRechargeOrders.vue
+++ src/pages/dashboard/tables/LatestRechargeOrders.vue
@@ -1,14 +1,16 @@
 <template>
-  <el-table :data="orders" style="width: 100%">
-    <el-table-column prop="orderNo" label="订单号">
+  <el-table :data="orders" style="width: 100%; text-align:center; vertical-align:middle;">
+    <el-table-column prop="orderNo" label="订单号" align='center'>
     </el-table-column>
-    <el-table-column prop="nickName" label="用户昵称">
+    <el-table-column prop="userId" label="用户ID" align='center'>
     </el-table-column>
-    <el-table-column prop="diamond" label="钻石数量">
+    <el-table-column prop="nickName" label="用户昵称" align='center'>
     </el-table-column>
-    <el-table-column prop="payName" label="充值方式">
+    <el-table-column prop="diamond" label="钻石数量" align='center'>
     </el-table-column>
-    <el-table-column prop="createTime" label="充值时间">
+    <el-table-column prop="payName" label="充值方式" align='center'>
+    </el-table-column>
+    <el-table-column prop="createTime" label="充值时间" align='center'>
     </el-table-column>
   </el-table>
 </template>
